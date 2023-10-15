cae9a3e89e7ef29743b35a7cc2d5233f38cfc368
bailixing <bailixing@pinduoduo.com>
2023-10-15 20:03:20 +0800

    feat(): 5.32 init1

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -1,19 +1,14 @@
 import Canvas from '@antv/f-react';
 import { Chart, Interval } from '@antv/f2'
+import { mockDataSource } from '../../mock-data';
 
-const data = [
-    { genre: 'Sports', sold: 275 },
-    { genre: 'Strategy', sold: 115 },
-    { genre: 'Action', sold: 120 },
-    { genre: 'Shooter', sold: 350 },
-    { genre: 'Other', sold: 150 },
-  ];
 
+const dataSource = mockDataSource
 const Bar = () => {
   return (
    <>
      <Canvas pixelRatio={window.devicePixelRatio}>
-      <Chart data={data} style={{padding: [0, 0, 0, 0]}}>
+      <Chart data={dataSource} style={{padding: [0, 0, 0, 0]}}>
         <Interval x="genre" y="sold" />
       </Chart>
     </Canvas>
